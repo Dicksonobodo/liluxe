84805e1bafc764bf4ef4a92e3201e8bd09b79c3d
Your Name <you@example.com>
2026-02-21 23:02:18 +0100

    sixe selector border radius reduced

--- src/components/product/SizeSelector.jsx
+++ src/components/product/SizeSelector.jsx
@@ -18,7 +18,7 @@ const SizeSelector = ({ sizes, selectedSize, onSelectSize, error }) => {
               onClick={() => !isOutOfStock && onSelectSize(size)}
               disabled={isOutOfStock}
               className={`
-                  px-1 py-1 border text-sm font-medium transition-all rounded
+                  px-0.5 py-1 border text-sm font-medium transition-all rounded
 
                 ${isSelected 
                   ? 'border-stone-900 bg-stone-900 text-white' 
