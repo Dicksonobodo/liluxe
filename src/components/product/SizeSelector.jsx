cb294691ef0cf77b595d693fb8570b6052b45a53
Your Name <you@example.com>
2026-02-21 23:36:00 +0100

    sixe selector border radius reduced

--- src/components/product/SizeSelector.jsx
+++ src/components/product/SizeSelector.jsx
@@ -7,7 +7,7 @@ const SizeSelector = ({ sizes, selectedSize, onSelectSize, error }) => {
         Select Size
       </label>
       
-      <div className="grid grid-cols-5 md:grid-cols-8 gap-1">
+      <div className="grid grid-cols-5 md:grid-cols-8 gap-6">
         {sizes.map(({ size, stock }) => {
           const isOutOfStock = stock === 0;
           const isSelected = selectedSize === size;
