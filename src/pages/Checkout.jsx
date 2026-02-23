6e40176fd359daa028e8ca78757c5c6b253b914b
Your Name <you@example.com>
2026-02-23 09:31:43 +0100

    added new important info

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -245,7 +245,7 @@ Order placed on ${new Date().toLocaleString('en-NG', {
                   <svg className="w-5 h-5 text-stone-400 flex-shrink-0 mt-0.5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                   </svg>
-                  Payment will be collected upon delivery or can be arranged beforehand.
+                  Payments will be made as soon as we contact you after making this order so we start processing your order.
                 </li>
                 <li className="flex items-start gap-2">
                   <svg className="w-5 h-5 text-stone-400 flex-shrink-0 mt-0.5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
